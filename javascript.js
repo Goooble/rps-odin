cfbbf58ad9c7460b71a9b0823c6acc809c9f159c
Gooble <bivinbheemaiah@gmail.com>
2024-07-04 13:49:13 +0530

    Compute one round winner
    for no reason i went with the complicated way for
    fun though i could have gotten it easier with
    if else but its not cool so yeah

--- javascript.js
+++ javascript.js
@@ -1,10 +1,112 @@
 // GET computer choice
-function getComputerChoice(){
-    return Math.floor(Math.random() * 3) + 1;
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3) + 1;
 }
 
 //GET human choice
-function getHumanChoice(){
-    return prompt("Enter your weapon", "here");
+function getHumanChoice() {
+  return prompt("Enter your weapon", "here");
 }
 
+//string to number converter
+function stringToNum(string) {
+  switch (string) {
+    case "rock":
+      return 1;
+      break;
+    case "paper":
+      return 2;
+      break;
+    case "scissor":
+      return 3;
+      break;
+    default:
+      return 100;
+      break;
+  }
+}
+
+//number to string converter
+function numToString(num) {
+  switch (num) {
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissor";
+      break;
+  }
+}
+
+//finding the greater number
+function getGreater(a, b) {
+  return a > b ? true : false;
+}
+
+//one round
+function playRound(humanChoice, computerChoice) {
+  console.log(`human: ${numToString(humanChoice)}`);
+  console.log(`computer: ${numToString(computerChoice)}`);
+  let winner;
+  //rock = 1
+  //paper = 2
+  //scissors = 3;
+  //three cases: rock and paper(3), paper and scissors(5), rock and scissors(4)
+  if(humanChoice === computerChoice){
+    return "Its a Draw!";
+  }
+  switch (humanChoice + computerChoice) {
+    case 3:
+    case 5: //pass the choices to a greatness checker
+      winner = getGreater(humanChoice, computerChoice);
+      break;
+
+    case 4: //put a negation to 4 when the boolean is returned
+      winner = !getGreater(humanChoice, computerChoice);
+      break;
+  }
+  //  Compute winner
+  return winner
+    ? `You win! ${numToString(humanChoice)} beats ${numToString(
+        computerChoice
+      )}!`
+    : `You lose! ${numToString(computerChoice)} beats ${numToString(
+        humanChoice
+      )}!`;
+  //if true = human wins and false computer wins
+}
+////////////////// MAINNNNN
+
+
+
+
+//score variables
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(playRound(
+  stringToNum(getHumanChoice().toLowerCase()),
+  getComputerChoice()
+));
+
+
+/*
+    convert humanChoice to the corresponding number
+    Problem: Deciding the winner-so i need to compare the humanchoice to the computer choice and calculate who the winner is 
+    input: corresponding numbers
+    three possible situations: draw, win and lose
+    draw condition is if the choices are equal, that leaves us to other numbers to work with 
+    CHECK for equal choice
+    CHECK if humanChoice = 1
+      if computerCHoice = 2, computer wins
+      else human wins
+    CHECK if humanChoice is 2
+      if computerChoice = 3, computer wins
+      else human wins
+    CHECK if humanChoice is 3
+      if computerChoice = 1, computer wins
+      else human wins
+  */
